f1e0cb145c354c2fa1b113332bf4968aec335012
Dominic Kasanga <dominickasanga@gmail.com>
2023-05-22 18:22:08 +0200

    modified:   autodep.py new file:   transpoter.py new file:   utils/decorators.py

---
+++ transpoter.py
@@ -0,0 +1,12 @@
+from utils import imp_exp_func, host, decorators, net
+import os
+from dotenv import load_dotenv
+import asyncio
+load_dotenv()
+
+@decorators.check_if_host_is_reachable
+def update_host(ip_address: str, user_name: str, headers: dict) -> bool:
+
+    details = asyncio.run(host.update_remote_host(user_name, ip_address))
+
+    print(details)
