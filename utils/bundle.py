0e2b670169ee29e5d23bb2c6926740696409d6a4
Dominic Kasanga <dominickasanga@gmail.com>
2023-05-24 18:19:04 +0200

    modified:   utils/bundle.py

--- utils/bundle.py
+++ utils/bundle.py
@@ -7,18 +7,20 @@ async def find_bundle_dir(client: AsyncParamikoSSHClient):
     home_dir = await client.send_command("echo $HOME")
     home_dir = home_dir.decode("utf-8")
 
-    bundle_dir = os.path.join(home_dir)
-    command    = f"find {bundle_dir} -name bundle"
-    stdout     = await client.send_command(command)
-    results    = stdout.readlines()
-    results    = [result.strip() for result in results]
+    return home_dir
+
+    # bundle_dir = os.path.join(home_dir)
+    # command    = f"find {bundle_dir} -name bundle"
+    # stdout     = await client.send_command(command)
+    # results    = stdout.readlines()
+    # results    = [result.strip() for result in results]
     
-    matching_dirs = []
-    for bundle_dir in results:
-        command = f"grep -liE 'rvm|rbenv' {bundle_dir}/*"
-        stdout  = await client.send_command(command)
-        rvm_rbenv_found = any(['rvm' in line.lower() or 'rbenv' in line.lower() for line in stdout.readlines()])
-        if not rvm_rbenv_found:
-            matching_dirs.append(bundle_dir)
+    # matching_dirs = []
+    # for bundle_dir in results:
+    #     command = f"grep -liE 'rvm|rbenv' {bundle_dir}/*"
+    #     stdout  = await client.send_command(command)
+    #     rvm_rbenv_found = any(['rvm' in line.lower() or 'rbenv' in line.lower() for line in stdout.readlines()])
+    #     if not rvm_rbenv_found:
+    #         matching_dirs.append(bundle_dir)
     
-    return matching_dirs
+    # return matching_dirs
