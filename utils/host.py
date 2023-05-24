64650edaa1134fd9030fdef0a48be3353902a999
Dominic Kasanga <dominickasanga@gmail.com>
2023-05-24 15:44:54 +0200

    modified:   utils/host.py

--- utils/host.py
+++ utils/host.py
@@ -5,6 +5,7 @@ import os
 import asyncio
 from .net import AsyncParamikoSSHClient, RedisCls
 from .app_version import getTag, instruction_set, generate_git_url
+import logging
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -27,29 +28,34 @@ async def update_remote_host(user_name: str, ip_address: str) -> str:
                 apps = []
 
 
+                # Set up logging
+                logging.basicConfig(level=logging.DEBUG)  # Configure logging to display debug-level messages
+
                 for app_dir in app_dirs:
                     git_pull_cmd = f"cd {app_dir} && git pull --tags http://{os.getenv('GIT_HOST')}:{generate_git_url(app_dir)}"
-                    print(f"Git Pull Command: {git_pull_cmd}")
-                    git_pull_stdout = await client.send_command(git_pull_cmd)
-                    git_pull_decoded_stdout = git_pull_stdout.decode("utf-8")
-                    print(f"Git Pull Output:\n{git_pull_decoded_stdout}")
+                    logging.debug(f"Git Pull Command: {git_pull_cmd}")
+                    stdout = await client.send_command(git_pull_cmd)
+                    decoded_stdout = stdout.decode("utf-8")  # Decode the stdout bytes into a string
+                    logging.debug(f"Git Pull Output:\n{decoded_stdout}")
 
-                    result = git_pull_stdout.splitlines()
+                    result = decoded_stdout.splitlines()
 
                     tag = getTag(app_dir=app_dir)
                     if tag:
                         git_checkout_cmd = f"cd {app_dir} && git checkout {tag} -f"
-                        print(git_checkout_cmd)
-                        checkout_stdout = await client.send_command(git_checkout_cmd)
-                        checkout_decoded_stdout = checkout_stdout
-                        print(f"Git Checkout Output:\n{checkout_decoded_stdout}")
+                        logging.debug(f"Git Checkout Command: {git_checkout_cmd}")
+                        stdout = await client.send_command(git_checkout_cmd)
+                        decoded_stdout = stdout.decode("utf-8")  # Decode the stdout bytes into a string
+                        logging.debug(f"Git Checkout Output:\n{decoded_stdout}")
 
-                        # if "BHT-EMR-API" in app_dir:
-                        #     for instruction in instruction_set:
-                        #         _cmd_ = f"cd {app_dir} && {instruction}"
-                        #         stdout = await client.send_command(_cmd_)
-                        #         decoded_stdout = stdout.decode("utf-8")
-                        #         print(f"{instruction} Output:\n{decoded_stdout}")
+                        if "BHT-EMR-API" in app_dir:
+                            for instruction in instruction_set:
+                                _cmd_ = f"cd {app_dir} && {instruction}"
+                                logging.debug(f"Instruction Command: {_cmd_}")
+                                stdout = await client.send_command(_cmd_)
+                                decoded_stdout = stdout.decode("utf-8")  # Decode the stdout bytes into a string
+                                logging.debug(f"{instruction} Output:\n{decoded_stdout}")
+                                # Add a logging statement here to display the stdout of the instruction
 
                     collection.append(result)
 
