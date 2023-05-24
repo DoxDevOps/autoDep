4a779461cfd9b0a2c4ca4e176e535db19ec2f7de
Dominic Kasanga <dominickasanga@gmail.com>
2023-05-24 18:42:42 +0200

    modified:   utils/bundle.py

--- utils/bundle.py
+++ utils/bundle.py
@@ -1,17 +1,22 @@
 from .net import AsyncParamikoSSHClient
 import os
+import asyncio
 
 async def find_bundle_dir(client: AsyncParamikoSSHClient):
+    try:
+        home_dir = await client.send_command("echo $HOME")
+        home_dir = home_dir.decode("utf-8").strip()
 
+        command = f"find {home_dir} -type d -name 'bundle'"
+        print(command)
 
-    home_dir = await client.send_command("echo $HOME")
-    home_dir = home_dir.decode("utf-8")
+        stdout = await client.send_command(command)
+        results = stdout.decode("utf-8").strip()
 
-    command      = "find "+home_dir+" -name bundle"
-    print(command)
-    stdout     = await client.send_command(command)
-    results    = stdout.decode("utf-8")
-    return command
+        return results
+
+    except Exception as e:
+        print(f"An error occurred in fn(find_bundle_dir): {str(e)}")
     # results    = [result.strip() for result in results]
     
     # matching_dirs = []
