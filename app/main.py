1dd9bc9e17aceb1154f611a852f2002af745737e
dominickasanga <dominickasanga@gmail.com>
2023-05-26 16:06:08 +0200

    renamed:    utils/__init__.py -> app/__init__.py new file:   app/autodeploy.py modified:   app/main.py modified:   app/templates/landing.html named:    transpoter.py -> app/transpoter.py new file:   app/utils/__init__.py renamed:    utils/app_version.py -> app/utils/app_version.py renamed:    utils/decorators.py -> app/utils/decorators.py renamed:    utils/host.py -> app/utils/host.py renamed:    utils/imp_exp_func.py -> app/utils/imp_exp_func.py renamed:    utils/net.py -> app/utils/net.py renamed:    utils/remote_exec_app.py -> app/utils/remote_exec_app.py deleted:    autodep.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from multiprocessing import Process, Value
+from autodeploy import init
+
+process = None
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -19,10 +23,17 @@ def login():
 
     return render_template('login.html')
 
-@app.route('/landing')
+@app.route('/landing', methods=['GET', 'POST'])
 def landing():
     if 'logged_in' in session and session['logged_in']:
-        return render_template('landing.html')
+        if request.method == 'POST':
+            if 'start' in request.form:
+                start_process()
+            elif 'stop' in request.form:
+                stop_process()
+
+        process_running_value = process.is_alive() if process is not None else False
+        return render_template('landing.html', process_running=process_running_value)
     else:
         return redirect(url_for('login'))
 
@@ -30,6 +41,24 @@ def landing():
 def logout():
     session.pop('logged_in', None)
     return redirect(url_for('login'))
+
+def start_process():
+    global process
+
+    if process is None or not process.is_alive():
+        # Start the process if it's not already running or terminated
+        process = Process(target=init)
+        process.start()
+        session['process_running'] = True
+
+def stop_process():
+    global process
+
+    if process is not None and process.is_alive():
+        # Stop the process if it's running
+        process.terminate()
+        process.join()
+        session['process_running'] = False
 
 if __name__ == '__main__':
     app.run(debug=True)
